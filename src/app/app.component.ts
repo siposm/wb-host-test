36eb43f05504701e3671ba2f696eacea2ded161c
Sipos Miklos <commit@siposm.hu>
2026-06-08 18:28:32 +0200

    working 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
-  title = 'hosting-demo';
+export class AppComponent implements OnInit {
+	title = "Hosting Demo"
+	todos: string[] = []
+	words: string[] = []
+	newTodo: string = ""
+
+	constructor(private http: HttpClient) {}
+
+	download(): void {
+		this.http.get<string[]>("https://api.siposm.hu/word").subscribe(x => {
+			console.log(x)
+			this.words = x
+		})
+	}
+
+	add(): void {
+		this.todos.push(this.newTodo)
+		this.newTodo = ""
+	}
+
+	ngOnInit(): void {
+		this.todos.push("Kenyeret kell venni","Csomagolás kirándulásra","Kutyát meg kell etetni")
+	}
 }
